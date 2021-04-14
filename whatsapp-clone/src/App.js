361f255983a3d857f237bd862074bcb9360ba4e3
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-14 11:18:03 -0300

    window primary division

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,9 +1,21 @@
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-   <>
-   </>
+
+    <div className='app-window'>
+
+      <div className='sidebar'>
+        Pedro>()...
+      </div>
+
+      <div className='content-area'>
+        ...pedro>()
+      </div>
+
+    </div>
+
   );
 }
 
