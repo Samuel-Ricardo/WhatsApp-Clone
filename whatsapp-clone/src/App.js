809dfafce9097ef75b293b559030392bac31496f
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-14 12:09:17 -0300

    style

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
           <img className='img-perfil' src='https://www.flaticon.com/svg/vstatic/svg/149/149071.svg?token=exp=1618412728~hmac=c56c9517cc5179914f240a60d068c9e6' />
 
-          <div className='icons'>
+          <div className='buttons'>
             <img src=''/>
           </div>
 
