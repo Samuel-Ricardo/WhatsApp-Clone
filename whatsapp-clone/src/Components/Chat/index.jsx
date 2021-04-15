5e96b68dbeb3fe1b1a4c37145058f4a6ad138722
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-15 12:24:06 -0300

    Create: Component - Chat

---
+++ whatsapp-clone/src/Components/Chat/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Chat.css'
+
+export default (props) => {
+
+
+
+
+  return (
+
+    <div>
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+    </div>
+
+  )
+}
