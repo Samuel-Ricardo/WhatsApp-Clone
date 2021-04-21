17c5a9b3a90884eca6d15c50633800b03924ff10
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 17:47:57 -0300

    firebase

---
+++ whatsapp-clone/src/firebase_config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyB27158ai7gdqKGFxpHtFQIfhnYtNbVfmE",
+  authDomain: "whatsapp-2-a85f3.firebaseapp.com",
+  projectId: "whatsapp-2-a85f3",
+  storageBucket: "whatsapp-2-a85f3.appspot.com",
+  messagingSenderId: "596771972223",
+  appId: "1:596771972223:web:537307414d253749e28b4a",
+  measurementId: "G-N8LSV7PKW8"
+};
+
