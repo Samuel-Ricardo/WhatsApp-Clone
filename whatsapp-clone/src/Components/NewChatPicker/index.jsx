0497cdd726fd8c695e9bf3cc890790363d76fad5
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 17:31:32 -0300

    conctact card

--- whatsapp-clone/src/Components/NewChatPicker/index.jsx
+++ whatsapp-clone/src/Components/NewChatPicker/index.jsx
@@ -6,34 +6,57 @@ import colors from '../../util/colors';
 
 
 
-export default ({contacts, user, isShowing, setShowing}) => {
+export default ({ contacts, user, isShowing, setShowing }) => {
 
   const [contactList, setcontactList] = useState(contacts)
 
+
+  const close = () => {
+
+    setShowing(false)
+  }
+
   return (
 
-    <selection className='NewChatPicker'>
+    <selection className='NewChatPicker'
+
+      style={{ left: isShowing ? "0px" : "-415px" }}
+    >
 
       <header>
 
-        <div clasName="button-group">
+        <div className="button-group">
 
-          <div clasName="button">
+          <div className="button" onClick={close}>
 
-            <ArrowBackIcon style={{color: "#FFFFFF"}}/>
+            <ArrowBackIcon style={{ color: "#FFFFFF" }} />
 
           </div>
         </div>
-        
-          <div className='title'>
-            <p>Novo Chat</p>
-          </div>
+
+        <div className='title'>
+          <p>Novo Chat</p>
+        </div>
 
 
 
       </header>
 
-      <section className='contact-list'>
+      <section className='chatlist'>
+
+        {contactList.map((item, key) => (
+
+          <div className="contact-card" key={key}>
+
+            <img className="avatar" src={item.image} />
+
+            <div className="name">
+              <p>{item.name}</p>
+            </div>
+
+          </div>
+
+        ))}
 
       </section>
 
