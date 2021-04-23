76a3f78bc0ef761155897a9e551de13573cadc05
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-23 20:05:28 -0300

    login with facebook

--- whatsapp-clone/src/server/API.js
+++ whatsapp-clone/src/server/API.js
@@ -6,3 +6,12 @@ import firebase_config from './firebase_config';
 
 const firebaseapp = firebase.initializeApp(firebase_config)
 const database = firebase.firestore()
+
+export default {
+  fbPopup: async () => {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    let result = await firebase.auth().signInWithPopup(provider)
+
+    return result;
+  }
+}
