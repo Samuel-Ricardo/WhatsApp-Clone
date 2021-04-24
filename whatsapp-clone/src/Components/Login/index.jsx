10c060edf559ab401a76dc97ae7c9562f508bf74
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-24 10:52:30 -0300

    create: body

--- whatsapp-clone/src/Components/Login/index.jsx
+++ whatsapp-clone/src/Components/Login/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./Login.css";
 
+import API from "../../server/API"
+
+
 
 export default () => {
 
   return (
-    <></>
+    <div className="login">
+      <button onClick={hancleFacebookLogin}> Login With Facebook </button>
+    </div>
   )
 }
